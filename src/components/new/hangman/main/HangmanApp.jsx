ec1652727ceed127b32c2da8afbbe1e97d0d5ed7
mirza salkovic <salkovic.mirza@gmail.com>
2022-09-23 11:38:43 +0200

    ad header and figure

---
+++ src/components/new/hangman/main/HangmanApp.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+import Figure from "../figure/Figure";
+import Header from "../../header/Header";
+
+const Hangman = () => {
+  return (
+    <div className="main">
+      <Header />
+      <Figure />
+    </div>
+  );
+};
+export default Hangman;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NotesApp from "./components/new/NotesApp";
-import Context from "./components/new/Context";
+import Hangman from "./components/new/hangman/main/HangmanApp";
 
 reportWebVitals();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Context>
-    <NotesApp />
-  </Context>
+  <>
+    <Hangman />
+  </>
 );
