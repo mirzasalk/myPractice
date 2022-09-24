a7a2d1589c20393cf5597eacdeba1ac871a5bd6a
mirza salkovic <salkovic.mirza@gmail.com>
2022-09-24 11:36:57 +0200

    change header

---
+++ src/components/new/hangman/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div id="header">
+      <h1>Hangman</h1>
+      <h4>Find the hiden word - Enter a letter</h4>
+    </div>
+  );
+};
+
+export default Header;
