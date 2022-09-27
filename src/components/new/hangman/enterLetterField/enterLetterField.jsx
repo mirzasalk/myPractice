69a0071629f149a428352c7cedd02c65b8db78c1
mirza salkovic <salkovic.mirza@gmail.com>
2022-09-27 21:38:32 +0200

    final changes

--- src/components/new/hangman/enterLetterField/enterLetterField.jsx
+++ src/components/new/hangman/enterLetterField/enterLetterField.jsx
@@ -24,31 +24,42 @@ const EnterLetterField = () => {
   }
 
   return (
-    <div className="bodyWrongLetter">
-      <input
-        className="inputForNextLetter"
-        type="text"
-        value={helpLetter}
-        placeholder="Enter Next Letter"
-        onChange={(e) => {
-          setHelpLetter(e.target.value);
-        }}
-        onKeyPress={(e) => {
-          if (e.key === "Enter" && !word.includes(e.target.value)) {
-            setWrongLetters([...wrongLetters, e.target.value]);
-            setHelpLetter("");
-            setEror(eror + 1);
-          } else if (e.key === "Enter") {
-            setCorectLetters([...corectLetters, e.target.value]);
-            setHelpLetter("");
-            setCorect(corect + 1);
-          }
-        }}
-      />
-      <div className="wrongLettersText">
-        <h3>Wrong Letters:</h3>
-        <div className="wrongLetterList">{wrongLetters}</div>
-      </div>
+    <div>
+      {hidenWord.length === 0 ? null : (
+        <div className="bodyWrongLetter">
+          <input
+            className="inputForNextLetter"
+            type="text"
+            value={helpLetter}
+            placeholder="Enter Next Letter"
+            onChange={(e) => {
+              if (helpLetter.length < 1 || e.key === "delete") {
+                setHelpLetter(e.target.value);
+              }
+            }}
+            onKeyPress={(e) => {
+              console.log(e.key);
+              if (e.key === "Enter" && !word.includes(e.target.value)) {
+                setWrongLetters([...wrongLetters, e.target.value]);
+                setHelpLetter("");
+                setEror(eror + 1);
+              } else if (e.key === "Enter") {
+                setCorectLetters([...corectLetters, e.target.value]);
+                setHelpLetter("");
+                if (corect === -1) {
+                  setCorect(corect + 2);
+                } else {
+                  setCorect(corect + 1);
+                }
+              }
+            }}
+          />
+          <div className="wrongLettersText">
+            <h3>Wrong Letters:</h3>
+            <div className="wrongLetterList">{wrongLetters}</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
